65b16b2378374fe0e6f3d23d77963911da31bba9
Suyog Magar <hkmagar11@gmail.com>
2025-09-25 12:24:37 +0530

    Pushed Base Backend Files

---
+++ src/main/java/com/realtime/realtimedatapipeline/RealTimeDataPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.realtime.realtimedatapipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeDataPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/realtime/realtimedatapipeline/RealTimeDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtime.realtimedatapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDataPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
